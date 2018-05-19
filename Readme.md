24780458919cefa2b1bffd186506238257056bfc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# (OBSOLETE) DashboardDesigner - How to customize the data source wizard to allow specifying a custom data source name
+
+
+<p>Update: Starting with version 15.2 it is possible to show the Name page by enabling the <a href="https://documentation.devexpress.com/#Dashboard/DevExpressDashboardWinDashboardDataSourceWizardSettings_ShowDataSourceNamePagetopic">DashboardDesigner.DataSourceWizardSettings.ShowDataSourceNamePage Property</a>.<br>This example demonstrates how to use the <a href="https://documentation.devexpress.com/#Dashboard/DevExpressDashboardWinDashboardDesigner_DataSourceWizardCustomizationtopic">DashboardDesigner.DataSourceWizardCustomization</a> to show the ChooseDataSourceNamePage that allows you to specify the resulting data source name as the first first wizard page. </p>
+
+<br/>
+
+
---
+++ VB/WizardCustomizationNamePage/DataSourceWizardCustomization.vb
@@ -0,0 +1,34 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon.DataSourceWizard
+Imports DevExpress.DashboardWin.ServiceModel
+Imports DevExpress.DataAccess.UI.Wizard
+Imports DevExpress.DataAccess.Wizard.Model
+Imports DevExpress.DataAccess.Wizard.Presenters
+Imports DevExpress.DataAccess.Wizard.Services
+Imports DevExpress.DataAccess.Wizard.Views
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace WizardCustomizationExample
+    Public Class DataSourceWizardCustomization
+        Implements IDashboardDataSourceWizardCustomization
+
+        Public Sub CustomizeDataSourceWizard(ByVal customization As IWizardCustomization(Of DashboardDataSourceModel)) Implements IDashboardDataSourceWizardCustomization.CustomizeDataSourceWizard
+            customization.StartPage = GetType(CustomChooseDataSourceNamePage(Of DashboardDataSourceModel))
+            customization.RegisterPage(Of CustomChooseDataSourceNamePage(Of DashboardDataSourceModel), CustomChooseDataSourceNamePage(Of DashboardDataSourceModel))()
+        End Sub
+    End Class
+    Public Class CustomChooseDataSourceNamePage(Of TModel As IDataSourceModel)
+        Inherits ChooseDataSourceNamePage(Of DashboardDataSourceModel)
+
+        Public Sub New(ByVal view As IChooseDataSourceNamePageView, ByVal dataSourceNameCreator As IDataSourceNameCreationService)
+            MyBase.New(view, dataSourceNameCreator)
+        End Sub
+        Public Overrides Function GetNextPageType() As Type
+            Return GetType(DashboardChooseDataSourceTypePage(Of DashboardDataSourceModel))
+        End Function
+    End Class
+End Namespace
